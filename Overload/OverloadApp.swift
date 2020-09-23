331536f691c5d63fdf176e2e8c5c1807afdbc127
Eric Schulte <triage@users.noreply.github.com>
2020-09-22 23:09:48 -0400

    Initial Commit

---
+++ Overload/OverloadApp.swift
@@ -0,0 +1,20 @@
+//
+//  OverloadApp.swift
+//  Overload
+//
+//  Created by Eric Schulte on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct OverloadApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
