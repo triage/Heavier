775c7f8eb738b2f80e33be0d8b82501696fe834d
Eric Schulte <triage@users.noreply.github.com>
2020-10-30 00:45:19 -0400

    wip

--- Overload/LiftView.swift
+++ Overload/LiftView.swift
@@ -12,18 +12,26 @@ struct LiftView: View {
     let lift: Lift?
     @State var selected: String = ""
     @State var reps: Int = 10
+    @State var sets: Int = 5
     var body: some View {
         VStack {
             HStack {
-                Picker("whatever", selection: $reps) {
-                    ForEach(0 ..< 100) {
-                        Text("\($0)")
-                            .sfCompactDisplay(.regular, size: 30.0)
+                ZStack {
+                    TransparentPicker(selection: $reps, rowCount: 10) { (row) in
+                        Text("\(row)")
+                            .sfCompactDisplay(.regular, size: 45.0)
+                            .padding(EdgeInsets(top: 15.0, leading: 0.0, bottom: 15.0, trailing: 0.0))
                     }
+                                        .frame(width: 30, height: 33)
+                                        .clipped()
+                    Path { path in
+                        path.move(to: CGPoint.zero)
+                        path.addLine(to: CGPoint(x: 60.0, y: 0.0))
+                    }.stroke(Color.blue, lineWidth: 3.0)
                 }
-                .frame(width: 100, height: 40)
-                .clipped()
-                Text("Reps").sfCompactDisplay(.regular, size: 30.0)
+
+                
+                Text("Reps").sfCompactDisplay(.regular, size: 45.0)
                 Spacer()
             }
         }.padding(30.0)
---
+++ Overload/TransparentPicker.swift
@@ -0,0 +1,90 @@
+//
+//  PickerTransparent.swift
+//  Overload
+//
+//  Created by Eric Schulte on 10/29/20.
+//
+
+import Foundation
+import SwiftUI
+
+final class TransparentPickerView: UIPickerView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        subviews[1].isHidden = true
+    }
+}
+
+/**
+ iOS Picker class with the update bug which can cause the SwiftUI picker to reset.
+ */
+struct TransparentPicker<Content: View>: UIViewRepresentable {
+    class Coordinator : NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+        @Binding var selection: Int
+        
+        var initialSelection: Int?
+        var viewForRow: (Int) -> Content
+        var rowCount: Int
+
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            rowCount
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            self.selection = row
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+            let hostingController = UIHostingController(rootView: viewForRow(row))
+            return hostingController.view
+        }
+        
+        init(selection: Binding<Int>, viewForRow: @escaping (Int) -> Content, rowCount: Int) {
+            self.viewForRow = viewForRow
+            self._selection = selection
+            self.rowCount = rowCount
+        }
+    }
+    
+    @Binding var selection: Int
+    
+    var rowCount: Int
+    let viewForRow: (Int) -> Content
+
+    func makeCoordinator() -> TransparentPicker.Coordinator {
+        return Coordinator(selection: $selection, viewForRow: viewForRow, rowCount: rowCount)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<TransparentPicker>) -> TransparentPickerView {
+        let view = TransparentPickerView()
+        view.delegate = context.coordinator
+        view.dataSource = context.coordinator
+        return view
+    }
+    
+    func updateUIView(_ uiView: TransparentPickerView, context: UIViewRepresentableContext<TransparentPicker>) {
+        
+        context.coordinator.viewForRow = self.viewForRow
+        context.coordinator.rowCount = rowCount
+
+        // This is the key part. If the updated value is the same as the one
+        // we started with, we just ignore it.
+        if context.coordinator.initialSelection != selection {
+            uiView.selectRow(selection, inComponent: 0, animated: true)
+            context.coordinator.initialSelection = selection
+        }
+    }
+}
+
+struct FastPicker_Previews: PreviewProvider {
+    @State static var selection = 0
+    static var previews: some View {
+        TransparentPicker(selection: $selection, rowCount: 20) { (row) in
+            Text("Row-\(row)")
+        }
+    }
+}
