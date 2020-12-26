58d50a93c96fcd1797b3284ee727a35f623e6a74
Eric Schulte <triage@users.noreply.github.com>
2020-12-26 16:05:26 -0400

    wip

--- Heavier/View/Screens/Lift/LiftView.swift
+++ Heavier/View/Screens/Lift/LiftView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import CoreData
+import Combine
 
 struct LiftViewCloseButton: View {
     let action: () -> Void
@@ -22,6 +23,8 @@ struct LiftViewCloseButton: View {
 
 struct DateButton: View {
     @Binding var date: Date
+    
+    private let datePickerPaddingLeading: CGFloat = 10.0
     
     struct ViewModel {
         static var dateFormatter: DateFormatter {
@@ -48,9 +51,23 @@ struct DateButton: View {
                     .foregroundColor(.liftDateForeground)
             }
             .padding(Theme.Spacing.medium)
-        }.background(Color(Color.Overload.liftDateBackground.rawValue))
+        }
+        .background(Color(Color.Overload.liftDateBackground.rawValue))
         .cornerRadius(Theme.Spacing.large)
         .padding([.bottom], Theme.Spacing.medium)
+        .padding([.leading], -datePickerPaddingLeading)
+        .accentColor(Color.accent)
+                .labelsHidden()
+        .padding(Theme.Spacing.medium)
+                .foregroundColor(Color.liftDateForeground)
+        .background(Color.clear)
+    }
+}
+
+class ObservableValue<Type: Any>: ObservableObject {
+    @Published var value: Type
+    init(value: Type) {
+        self.value = value
     }
 }
 
@@ -61,10 +78,12 @@ struct LiftView: View {
     @State var reps: Float
     @State var sets: Float
     @State var weight: Float
-    @State var date = Date()
-    @State var dateControlPresented: Bool = false
+    @State var dateControlPresented = false
+    @State var showDatePicker = false
     @Binding var presented: Bool
+    
     @Environment(\.managedObjectContext) var managedObjectContext
+    @ObservedObject var dateObserved = ObservableValue(value: Date())
     
     init(exercise: Exercise, lift: Lift?, presented: Binding<Bool>) {
         self.exercise = exercise
@@ -89,7 +108,7 @@ struct LiftView: View {
         lift.sets = Int16(sets)
         lift.weight = Float(Lift.normalize(weight: weight))
         lift.id = UUID()
-        lift.timestamp = Date()
+        lift.timestamp = dateObserved.value
         lift.exercise = exercise
         do {
             try? managedObjectContext.save()
@@ -97,22 +116,16 @@ struct LiftView: View {
         presented.toggle()
     }
     
-    private static let datePickerPaddingLeading: CGFloat = 10.0
-    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading, spacing: 18.0) {
                 MostRecentLift(lift: lift)
-//                DatePicker(selection: $date, displayedComponents: [.date]) {
-//                    Text("")
-//                }
-                DateButton(date: $date)
-                .padding([.leading], -LiftView.datePickerPaddingLeading)
-                .accentColor(Color.accent)
-                        .labelsHidden()
-                .padding(Theme.Spacing.medium)
-                        .foregroundColor(Color.liftDateForeground)
-                .background(Color.clear)
+                
+                Button(action: {
+                    showDatePicker.toggle()
+                }, label: {
+                    DateButton(date: $dateObserved.value)
+                }).zIndex(1)
                 
                 LiftPicker(
                     label: "sets",
@@ -120,28 +133,31 @@ struct LiftView: View {
                     interval: 1,
                     value: $sets,
                     initialValue: Float(lift?.sets ?? 1)
-                )
+                ).zIndex(0)
+                
                 LiftPicker(
                     label: "reps",
                     range: 1...30,
                     interval: 1,
                     value: $reps,
                     initialValue: Float(lift?.reps ?? 1)
-                )
+                ).zIndex(0)
+                
                 LiftPicker(
                     label: Settings.shared.units.label,
                     range: 0...Settings.shared.units.maxWeight,
                     interval: Settings.shared.units.interval,
                     value: $weight,
                     initialValue: Float(lift?.weightLocalized.weight ?? 1)
-                )
+                ).zIndex(0)
+                
                 HStack(alignment: .lastTextBaseline, spacing: Theme.Spacing.medium) {
                     Text(volumeText)
                         .sfCompactDisplay(.medium, size: Theme.Font.Size.giga)
                         .minimumScaleFactor(0.4)
                         .lineLimit(1)
                 }
-                
+
                 Button(action: {
                     save()
                 }, label: {
@@ -153,11 +169,22 @@ struct LiftView: View {
                 }).background(Color.blue)
                 .cornerRadius(Theme.Spacing.medium * 2.0)
                 .padding([.top], Theme.Spacing.large)
-                
+
                 Spacer()
                 
             }.padding([.top, .leading, .trailing], Theme.Spacing.large)
             .navigationTitle(exercise.name!)
+        }
+        .onReceive(dateObserved.$value, perform: { _ in
+            if showDatePicker {
+                showDatePicker.toggle()
+            }
+        })
+        .sheet(isPresented: $showDatePicker) {
+            DatePicker("", selection: $dateObserved.value, displayedComponents: [.date])
+                .labelsHidden()
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding(Theme.Spacing.medium)
         }
     }
 }
