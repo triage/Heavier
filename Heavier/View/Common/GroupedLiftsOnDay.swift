38b77243b1ae13665b76d613e8ca4179d8981126
Eric <peoplefromgoodhomes@gmail.com>
2022-08-25 21:41:11 -0400

    fix issue of text alignment with multi-line notes

--- Heavier/View/Common/GroupedLiftsOnDay.swift
+++ Heavier/View/Common/GroupedLiftsOnDay.swift
@@ -20,6 +20,7 @@ struct GroupedLiftsOnDay: View {
                         .sfCompactDisplay(.regular, size: Theme.Font.Size.mediumPlus)
                     if let notes = lifts.notes, notes.count > 0 {
                         Text(notes)
+                            .multilineTextAlignment(.leading)
                             .sfCompactDisplay(.regular, size: Theme.Font.Size.medium)
                             .padding([.bottom], Theme.Spacing.small)
                     }
@@ -28,3 +29,34 @@ struct GroupedLiftsOnDay: View {
         }
     }
 }
+
+struct GroupedLiftsOnDay_Preview: PreviewProvider {
+    static var previews: some View {
+        
+        Settings.shared.units = .metric
+        
+        let moc = PersistenceController.preview.container.viewContext
+        
+        let exercise = Exercise(context: moc)
+        exercise.name = "Romanian Deadlift"
+        exercise.id = UUID()
+        var lifts = [Lift]()
+        let secondsPerDay: TimeInterval = 60 * 60 * 24
+        for date in [Date(), Date().addingTimeInterval(secondsPerDay), Date().addingTimeInterval(secondsPerDay * 2)] {
+            for index in 0...3 {
+                let lift = Lift(context: moc)
+                lift.reps = 10 + Int16(index)
+                lift.sets = 1
+                lift.notes = "Middle\nHigh Outside"
+                lift.weight = 20
+                lift.id = UUID()
+                lift.timestamp = date
+                lifts.append(lift)
+            }
+        }
+        return NavigationView {
+            GroupedLiftsOnDay(groups: [lifts])
+        }.environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
